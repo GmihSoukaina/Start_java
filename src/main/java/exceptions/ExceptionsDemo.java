e3820bc535d4db319c0437c6033851c8fe22327e
GmihSoukaina <soukaina4125@gmail.com>
2025-01-21 21:45:02 +0100

    commit

--- src/main/java/exceptions/ExceptionsDemo.java
+++ src/main/java/exceptions/ExceptionsDemo.java
@@ -1,4 +1,13 @@
 package exceptions;
 
 public class ExceptionsDemo {
+    public static void show () {
+        sayHello(null);
+    }
+
+    public static void sayHello (String name){
+        System.out.println(name.toLowerCase());
+    }
+
+    // we have error NullPointerException becuse that we must handle runtime errors
 }
